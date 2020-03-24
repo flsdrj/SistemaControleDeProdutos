0a562b04033282719d35e692bccba0859fe050cc
flsdrj <flsd_rj@hotmail.com>
2020-03-24 20:41:24 -0300

    Arquivos do Projeto

---
+++ SQLQuery1.sql
@@ -0,0 +1,15 @@
+﻿Create table Estoque(
+IdEstoque	integer			identity(1,1),
+Nome		nvarchar(150)	not null,
+Descricao	nvarchar(MAX)	not null,
+primary key(IdEstoque))
+
+Create table Produto(
+IdProduto	integer			identity(1,1),
+Nome		nvarchar(150)	not null,
+Preco		decimal(18,2)	not null,
+Quantidade	integer			not null,
+IdEstoque	integer			not null,
+primary key(IdProduto),
+foreign Key(IdEstoque)
+	references Estoque(IdEstoque))
